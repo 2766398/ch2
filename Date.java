5342e1690e9e3f80a102efaa0f174fd80aa18379
2766398 <2766398@learn.vsb.bc.ca>
2026-09-14 14:31:04 -0700

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,10 @@
+public class Date{
+	public static void main (String[] args){
+		String day = "Monday";
+	    int date = 14;
+		String month = "September ";
+		int year = 2026;
+			System.out.println("American format: " + day + ", " + month + date + ", " + year);
+			System.out.println("European format: " + day + " " + date + " " + month + year);
+	}
+}
